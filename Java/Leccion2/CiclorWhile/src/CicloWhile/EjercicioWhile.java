2cac9040fb781004591ddf45a085468b4c230067
Franco <francopaganoo2015@gmail.com>
2023-08-22 17:39:42 -0300

    Clase 1 Ciclos - Ciclo For

--- Java/Leccion2/CiclorWhile/src/CicloWhile/EjercicioWhile.java
+++ Java/Leccion2/CiclorWhile/src/CicloWhile/EjercicioWhile.java
@@ -3,16 +3,21 @@ package CicloWhile;
 
 public class EjercicioWhile {
     public static void main(String[] args) {
+        // Ciclo While
         var conteo = 0; //Inferencia de tipos
         while(conteo < 7){
             System.out.println("conteo = "+conteo);
             conteo++; //Vamos a aumentar en un uno la variable 
         }
-        
+        //Ciclo Do While
         var contador = 0;
         do{
             System.out.println("contador = " + contador);
             contador ++;
         }while(contador < 7);
+        //Ciclo For
+        for(var contando = 0; contando < 7; contando++){
+            System.out.println("contando = " + contando);
+        }
     }
 }
