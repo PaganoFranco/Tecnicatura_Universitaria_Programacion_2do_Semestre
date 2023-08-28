0c0533d73c7460ea8adc9bed2dd07dc07e371666
Franco <francopaganoo2015@gmail.com>
2023-08-28 11:10:45 -0300

    Clase 2 Ciclos y Clase Scanner - Ejercicio 1 de ciclos sin Scanner

--- Java/Leccion2/EjerciciioCiclo01/src/Ciclos01/Ciclo01.java
+++ Java/Leccion2/EjerciciioCiclo01/src/Ciclos01/Ciclo01.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Ciclos01;
 
-/**
- *
- * @author Franc
- */
+import java.util.Scanner;
+
+/*
+Ejercicio 1: Leer un numero y comprar su cuadrado, repetir
+el proceso hasta que se instrduzca un numero negativo
+*/
+
 public class Ciclo01 {
-    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int numero, cuadrado;
+        System.out.println("Ingrese un numero: ");
+        numero = Integer.parseInt(entrada.nextLine());
+        while (numero >= 0){
+            cuadrado = (int)Math.pow(numero, 2);
+            System.out.println("El numero " + numero + " elevado al cuadrado es: "+ cuadrado);
+            System.out.println("Ingrese un numero: ");
+            numero = Integer.parseInt(entrada.nextLine());
+        }
+        System.out.println("El programa a finalizado por numero negativo");
+        
+    }
 }
+
