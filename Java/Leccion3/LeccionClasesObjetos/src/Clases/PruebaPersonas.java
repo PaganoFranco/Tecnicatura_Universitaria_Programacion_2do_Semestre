eeb4a624286f6b1c3054a54dcd41f72cd6458714
Franco <francopaganoo2015@gmail.com>
2023-09-09 20:22:21 -0300

    Clase 4 Clases y Objetos P2 - Creacion de un Objeto mas

--- Java/Leccion3/LeccionClasesObjetos/src/Clases/PruebaPersonas.java
+++ Java/Leccion3/LeccionClasesObjetos/src/Clases/PruebaPersonas.java
@@ -3,10 +3,14 @@ package Clases;
 
 public class PruebaPersonas {
     public static void main(String[] args) {
-        Persona persona1;
-        persona1 = new Persona(); //Llamamos al constructor
-        persona1.nombre = "Franco";
+        Persona persona1 = new Persona(); //Llamamos al constructor (Metodo especial)
+        persona1.nombre = "Franco"; //El valor hexadecimal normalmente comieza con 0x
         persona1.apellido = "Pagano";
         persona1.obtenerInformacion();
+        
+        Persona persona2 = new Persona();
+        System.out.println("persona2 = " + persona2);
+        System.out.println("persona1 = " + persona1);
+        persona2.obtenerInformacion();
     }
 }
