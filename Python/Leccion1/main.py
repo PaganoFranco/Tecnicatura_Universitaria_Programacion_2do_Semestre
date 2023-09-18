f8b6a1e1124c6b22afca801f3dab563cbdb080b2
Franco <francopaganoo2015@gmail.com>
2023-09-18 18:34:29 -0300

    Clase 3 colecciones parte 3 - COLA

--- Python/Leccion1/main.py
+++ Python/Leccion1/main.py
@@ -379,3 +379,17 @@ print(pila)
 elementoBorrado = pila.pop()  # Quita el ultimo elemento y lo guarda en la vairable
 print(f"Sacamos el elemtno: {elementoBorrado}")
 print(f"La pila ahora queda asi: {pila}")
+
+# Colas con listas
+# Estructura de datos de tipo fifo(first input / first output)
+cola = ["Ariel", "Osvaldo", "Liliana", "Pilar"]
+
+# Agregamos elementos al final de la cola
+cola.append("Natalia")
+cola.append("Jose")
+print(cola)
+
+# Sacar elementos de la cola
+seRetira = cola.pop(0)
+print(f"Atendido el cliente: {seRetira}")
+print(cola)
