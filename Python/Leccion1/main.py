f16f733c6eadb1e6caaf293066ce17a7fd252e81
Franco <francopaganoo2015@gmail.com>
2023-09-18 18:28:02 -0300

    Clase 3 colecciones parte 3 - PILAS

--- Python/Leccion1/main.py
+++ Python/Leccion1/main.py
@@ -367,5 +367,15 @@ for llave, valor in seleccionArgentina.items():
 print("Tenemos cargados en el diccionario una cantidad de jugadores: ", end=" ")
 print(len(seleccionArgentina))
 
+# Pilas isando listas
+pila = [1, 2, 3]
 
+# Agregamos elementos a la pila por el final
+pila.append(4)
+pila.append(5)
+print(pila)
 
+# Sacamos elementos desde el final
+elementoBorrado = pila.pop()  # Quita el ultimo elemento y lo guarda en la vairable
+print(f"Sacamos el elemtno: {elementoBorrado}")
+print(f"La pila ahora queda asi: {pila}")
