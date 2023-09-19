069a4e5a197d6cc7d967b9d6b5e04cb148b3e4d8
Franco <francopaganoo2015@gmail.com>
2023-09-19 11:28:53 -0300

    Clase 4 Ejercicios y Mas - Recorremos el diccionario

--- Python/Leccion1/main.py
+++ Python/Leccion1/main.py
@@ -393,3 +393,7 @@ print(cola)
 seRetira = cola.pop(0)
 print(f"Atendido el cliente: {seRetira}")
 print(cola)
+
+# Seguimos mostrando como recorrer un diccionario
+for i in seleccionArgentina:
+    print(f"{i} → {seleccionArgentina[i]}")
