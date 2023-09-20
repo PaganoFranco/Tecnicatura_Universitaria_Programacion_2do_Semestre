2ef9d374a859763935e1cb98d00d5081a0080797
Franco <francopaganoo2015@gmail.com>
2023-09-20 06:46:36 -0300

    Clase 5 Tipo de Datos Parte 3 - Dato tipo Undefined y null

--- JavaScript/Leccion 2/02-01-Tipos de datos en JavaScript.js
+++ JavaScript/Leccion 2/02-01-Tipos de datos en JavaScript.js
@@ -43,3 +43,14 @@ class Persona{
 }
 console.log(typeof Persona);
 
+//Tipo de dato undefined
+var x;
+console.log(typeof x);
+
+x = undefined;
+console.log(x);
+
+// null: significa ausencia de valor
+var y = null; //No es un tipo de dato, pero su origen es object
+console.log(typeof y);
+
