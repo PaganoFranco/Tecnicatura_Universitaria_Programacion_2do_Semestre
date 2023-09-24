8f05aedf42a6e4ef12a7efd648eefea3a3463d84
Franco <francopaganoo2015@gmail.com>
2023-09-24 20:46:12 -0300

    Clase 5 Clases y Objetos P3 - Clase aritmetica - Crear metodo - recorremos con Debbug

--- Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
+++ Java/Leccion4/Aritmetica/src/Operaciones/Aritmetica.java
@@ -11,4 +11,10 @@ public class Aritmetica {
         int resultado = a + b;
         System.out.println("resultado = " + resultado);
     }
+    
+    public int sumarConRetorno(){
+        int resultado = a + b;
+        return resultado;
+    }
 }
+
