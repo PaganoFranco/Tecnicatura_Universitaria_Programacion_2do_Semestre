193704627906d52b536c8e0972a54b0df5c3c60e
Franco <francopaganoo2015@gmail.com>
2023-09-25 23:16:51 -0300

    Clase 6 Funciones parte 2 - Funciones: Valores por Default en Argumentos

--- Python/Leccion2/main.py
+++ Python/Leccion2/main.py
@@ -52,3 +52,8 @@ def sumar(a, b):
 # resultado = sumar(78, 22)
 # print(f"El resultado de la suma es: {resultado}")
 print(f"El resultado de la suma es: {sumar(3,4)}")
+
+def sumar2(a = 0, b = 0): # Le damos un valor por default
+    return a + b
+resultado = sumar2()
+print(f"El resultado de la suma es: {resultado}")
