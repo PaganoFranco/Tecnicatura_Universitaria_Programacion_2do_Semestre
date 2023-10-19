d75608960dad0bf72cf0cfabb29c4b39d1ab2b9d
Franco <francopaganoo2015@gmail.com>
2023-10-19 10:51:10 -0300

    Clase 8 var, let y const - Ejercicio dentro de un rago: Operador AND

--- JavaScript/Leccion 3/03-01OperadoresJavaScript.js
+++ JavaScript/Leccion 3/03-01OperadoresJavaScript.js
@@ -16,3 +16,13 @@ if (edad > 18){
 else{
     console.log("Es menor de edad");
 }
+
+// Ejercicio: dentro de un rango
+let dentroRango = 5; //Aqui vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Esta adentro del rango establecido");
+} else {
+    console.log("Esta fuera del rango establecido");
+}
+
