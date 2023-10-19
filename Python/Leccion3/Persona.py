7bc12f4ddc16eec9c4d4f696053639e8001ab6f8
Franco <francopaganoo2015@gmail.com>
2023-10-19 09:38:53 -0300

    Clase 8 POO Parte 1 - Creacion de objetos con argumentos

--- Python/Leccion3/Persona.py
+++ Python/Leccion3/Persona.py
@@ -1,10 +1,10 @@
 class Persona:  # Creamos una clase
-    def __init__(self):  # Se lo llama metodo INIT DUNDER
-        self.nombre = "Franco"
-        self.apellido = "Pagano"
-        self.edad = 26
+    def __init__(self, nombre, apellido, edad):  # Se lo llama metodo INIT DUNDER
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
 
-persona1 = Persona()  # Constructor
+persona1 = Persona("Franco", "Pagano", 26)  # Necesitamos enviar argumentos
 print(persona1.nombre)
 print(persona1.apellido)
 print(persona1.edad)
