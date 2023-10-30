f41981307c1e3e07d2148ce89986d834a06bf9d6
Franco <francopaganoo2015@gmail.com>
2023-10-30 12:10:24 -0300

    Clase 8 PPO parte 6

--- Java/Leccion5/ContextoEstatico/src/test/PersonaPrueba.java
+++ Java/Leccion5/ContextoEstatico/src/test/PersonaPrueba.java
@@ -5,6 +5,8 @@ import domain.Persona;
 
 
 public class PersonaPrueba {
+    private int contador;
+    
     public static void main(String[] args) {
         Persona persona1 = new Persona("Franco");
         System.out.println("persona1 = " + persona1);
@@ -12,10 +14,18 @@ public class PersonaPrueba {
         System.out.println("persona2 = " + persona2);
         imprimir(persona1);
         imprimir(persona2);
+        //this.contador = 10; //No se puede referencias desde un contexto estatico
+        PersonaPrueba personaP1 = new PersonaPrueba();
+        System.out.println(personaP1.getContador());
     }
     
     static public void imprimir(Persona persona){
         System.out.println("persona = " + persona);
+    }
+    
+    public int getContador(){
+        imprimir(new Persona("Juan"));
+        return this.contador;
     }
 }
  
