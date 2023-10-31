42e9791814050dde83e55c8cb91cb76c5e242fd7
Franco <francopaganoo2015@gmail.com>
2023-10-31 00:58:46 -0300

    Clase 9 POO Herencia - Herencia parte 2

--- Java/Leccion6/Herencia/src/domain/Persona.java
+++ Java/Leccion6/Herencia/src/domain/Persona.java
@@ -1,13 +1,65 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package domain;
 
-/**
- *
- * @author Franc
- */
 public class Persona {
+    //Atributos de herencia
+    protected String nombre;
+    protected char gener;
+    protected int edad;
+    protected String direccion;
+    
+    //Constructor vacio: este es para crear objetos sin necesidad de inicializar
+    //los atributos de la clase
+    public Persona(){ //Constructor 1
+        
+    }
+    
+    public Persona(String nombre){ //Constructor 2
+        this.nombre = nombre;
+    }
+
+    public Persona(String nombre, char gener, int edad, String direccion) {
+        this.nombre = nombre;
+        this.gener = gener;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public char getGener() {
+        return this.gener;
+    }
+
+    public void setGener(char gener) {
+        this.gener = gener;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", gener=" + gener + ", edad=" + edad + ", direccion=" + direccion + '}';
+    }
     
 }
---
+++ Java/Leccion6/Herencia/src/test/TesteHerencia.java
@@ -0,0 +1,11 @@
+
+package test;
+
+import domain.Empleado;
+
+public class TesteHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
