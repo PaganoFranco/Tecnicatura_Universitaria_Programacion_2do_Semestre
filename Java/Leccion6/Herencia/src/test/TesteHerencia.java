a8bccfd2cc72c1e3b08283fd15d1356c09cf1dfe
Franco <francopaganoo2015@gmail.com>
2023-11-02 06:07:29 -0300

    Clase 9 POO Herencia - Herencia parte 5 - Clase cliente

--- Java/Leccion6/Herencia/src/test/TesteHerencia.java
+++ Java/Leccion6/Herencia/src/test/TesteHerencia.java
@@ -1,11 +1,17 @@
 
 package test;
 
+import domain.Cliente;
 import domain.Empleado;
+import java.util.Date;
 
 public class TesteHerencia {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Franco", 45000.0);
         System.out.println("empleado1 = " + empleado1);
+        
+        //Cremos el objeto de la clase cliente
+        Cliente cliente1 = new Cliente(new Date(), true, "Julieta", 'F', 27, "Av. Julio Roca 1801");
+        System.out.println("cliente1 = " + cliente1);
     }
 }
