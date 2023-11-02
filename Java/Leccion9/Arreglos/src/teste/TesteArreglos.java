63fd194cb5b47344c404c7b3c6193582b2535e1b
Franco <francopaganoo2015@gmail.com>
2023-11-02 09:09:40 -0300

    Clase 11 arreglos o arrays - arreglos parte 2

--- Java/Leccion9/Arreglos/src/teste/TesteArreglos.java
+++ Java/Leccion9/Arreglos/src/teste/TesteArreglos.java
@@ -8,5 +8,14 @@ public class TesteArreglos {
         //EL lado derecho instanciamos un objeto de tipo Object
         System.out.println("edades = " + edades);
         
+        //Modificar un elemento del arreglo
+        edades[0] = 17;
+        System.out.println("edades 0 = " + edades [0]);
+        
+        //Tarea 
+        edades[1] = 24;
+        System.out.println("edades 1 = " + edades[1]);
+        edades[2] = 2;
+        System.out.println("edades 2 = " + edades[2]);
     }
 }
