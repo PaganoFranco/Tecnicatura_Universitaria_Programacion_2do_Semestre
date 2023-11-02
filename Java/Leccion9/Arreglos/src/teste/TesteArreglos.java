9cffce7ee3421b74cef190bb2c5ca609aaa2637b
Franco <francopaganoo2015@gmail.com>
2023-11-02 09:06:25 -0300

    Clase 11 arreglos o arrays - creacion leccion 9

---
+++ Java/Leccion9/Arreglos/src/teste/TesteArreglos.java
@@ -0,0 +1,12 @@
+
+package teste;
+
+public class TesteArreglos {
+    public static void main(String[] args) {
+        int edades [] = new int[3]; 
+        //El lado izquiedo es donde declaramos la variable
+        //EL lado derecho instanciamos un objeto de tipo Object
+        System.out.println("edades = " + edades);
+        
+    }
+}
