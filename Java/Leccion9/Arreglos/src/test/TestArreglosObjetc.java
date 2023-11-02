0c46129cf55084cec7d983689c68098cc9aa107c
Franco <francopaganoo2015@gmail.com>
2023-11-02 11:20:29 -0300

    Clase 11 arreglos o arrays - Iterar en java

--- Java/Leccion9/Arreglos/src/test/TestArreglosObjetc.java
+++ Java/Leccion9/Arreglos/src/test/TestArreglosObjetc.java
@@ -8,9 +8,21 @@ public class TestArreglosObjetc {
         Persona personas [] = new Persona[2];
         personas[0] = new Persona("Fracno");
         personas[1] = new Persona("Julieta");
-        
         System.out.println("personas 0 = " + personas [0]);
         System.out.println("personas 1 = " + personas [1]);
+        
+        for(int i = 0; i < personas.length; i++){
+            System.out.println("personas "+i+" = " + personas[i]);
+        }
+        
+        //Trabajamos con arreglos en la sintaxis resumida
+        String frutas[] = {"Banana", "Pera", "Durazno"};
+        for(int i = 0; i < personas.length; i++){
+            System.out.println("Frutas = "+frutas [i]);
+        }
+        
+        
+        
         
     }
 }
