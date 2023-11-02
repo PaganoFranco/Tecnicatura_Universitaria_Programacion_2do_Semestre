a13fffef0e81c23d58855995eb0b627883b9105f
Franco <francopaganoo2015@gmail.com>
2023-11-02 06:16:25 -0300

    Clase 9 POO Herencia - Orden de construccion de objetos - Ejecucion paso a paso (debug)

--- Java/Leccion6/Herencia/src/test/TesteHerencia.java
+++ Java/Leccion6/Herencia/src/test/TesteHerencia.java
@@ -10,8 +10,10 @@ public class TesteHerencia {
         Empleado empleado1 = new Empleado("Franco", 45000.0);
         System.out.println("empleado1 = " + empleado1);
         
+        Date fecha1 = new Date();
+        
         //Cremos el objeto de la clase cliente
-        Cliente cliente1 = new Cliente(new Date(), true, "Julieta", 'F', 27, "Av. Julio Roca 1801");
+        Cliente cliente1 = new Cliente(fecha1, true, "Julieta", 'F', 27, "Av. Julio Roca 1801");
         System.out.println("cliente1 = " + cliente1);
         //RECORDATORIO EL CHAR VA CON COMILLAS SIMPLES
     }
