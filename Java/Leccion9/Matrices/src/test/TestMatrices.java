70d921ad09b052779132422e9f539bc85f4b0393
Franco <francopaganoo2015@gmail.com>
2023-11-02 22:52:15 -0300

    Clase 12 Matrices - Ciclo for iterando

--- Java/Leccion9/Matrices/src/test/TestMatrices.java
+++ Java/Leccion9/Matrices/src/test/TestMatrices.java
@@ -10,11 +10,23 @@ public class TestMatrices {
         edades[0][1] = 7; //Es una diferente columna
         edades[1][0] = 8; 
         edades[1][1] = 4;
+        edades[2][0] = 4;
+        edades[2][1] = 4;
+       
         
         System.out.println("edades 0 * 0 = " + edades[0][0]);
         System.out.println("edades 0 * 1 = " + edades[0][1]);
         System.out.println("edades 1 * 0 = " + edades[1][0]);
         System.out.println("edades 1 * 1 = " + edades[1][1]);
+        System.out.println("edades 2 * 0 = " + edades[2][0]);
+        System.out.println("edades 2 * 1 = " + edades[2][1]);
         
+        
+        for (int fila = 0; fila < edades.length; fila++) {
+            for (int col = 0; col < edades[fila].length; col++) {
+                System.out.println("edades " + fila+"-"+col+ ": " + edades[fila][col]);
+            }
+            
+        }
     }
 }
