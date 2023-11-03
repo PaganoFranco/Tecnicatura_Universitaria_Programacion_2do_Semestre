db1c51037ffe7100af15d6a0952946c2446a6bbe
Franco <francopaganoo2015@gmail.com>
2023-11-03 12:18:54 -0300

    Clase 9 POO parte 2 - Clase Aritmetica Resta, multiplicacion y division

--- Python/Leccion3/Artimetica.py
+++ Python/Leccion3/Artimetica.py
@@ -13,6 +13,18 @@ class Aritmetica:
     def sumar(self):
         return self.operandoA + self.operandoB
 
+    def restar(self):
+        return self.operandoA - self.operandoB
+
+    def multiplicar(self):
+        return self.operandoA * self.operandoB
+
+    def dividir(self):
+        return self.operandoA / self.operandoB
 
 aritmetica1 = Aritmetica(7, 9)  # Le pasamos los argumentos para los operadores
 print(aritmetica1.sumar())
+
+print(f"La resta de los numeros es: {aritmetica1.restar()}")
+print(f"La multiplicacion de los numeros es: {aritmetica1.multiplicar()}")
+print(f"la division de los numeros es: {aritmetica1.dividir():.2f}")
