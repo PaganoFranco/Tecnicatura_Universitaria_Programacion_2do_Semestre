d5a187b6bc8600e52d47cf87e799ae7831422f91
Franco <francopaganoo2015@gmail.com>
2023-11-03 23:26:23 -0300

    Clase 10 POO parte 3 Metodos set & get

---
+++ Python/Leccion3/Persona2.py
@@ -0,0 +1,42 @@
+class Persona2:
+    def __init__(self, nombre, apellido, edad): # Esta encapsulado
+        self._nombre = nombre
+        self._apellido = apellido
+        self._edad = edad
+
+    def mostrar_detalles(self):
+        print(f"Los datos a mostrar son los siguientes: {self.nombre} {self.apellido} {self.edad}")
+    @property # decorador
+    def nombre(self): # Metodo getter
+        print("Estamos utilizando el metodo get")
+        return self._nombre
+    @nombre.setter
+    def nombre(self, nombre): # Metodo setter
+        print("Estamos utilizando el metodo set")
+        self._nombre = nombre
+
+    @property  # decorador
+    def apellido(self):  # Metodo getter
+        # print("Estamos utilizando el metodo get")
+        return self._apellido
+
+    @apellido.setter
+    def apellido(self, apellido):  # Metodo setter
+        #print("Estamos utilizando el metodo set")
+        self._apellido = apellido
+
+    @property  # decorador
+    def edad(self):  # Metodo getter
+        # print("Estamos utilizando el metodo get")
+        return self._edad
+
+    @edad.setter
+    def edad(self, edad):  # Metodo setter
+        # print("Estamos utilizando el metodo set")
+        self._edad = edad
+
+persona1 = Persona2("Franco", "Pagano", 26)
+print(persona1.nombre) # de esta forma llamamos al metodo getter
+persona1.nombre = "Juan Pablo"
+print(persona1.nombre)
+print(persona1.mostrar_detalles())
