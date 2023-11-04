d7f58dec88d36e8f318e0df6a37a92dc0478fe13
Franco <francopaganoo2015@gmail.com>
2023-11-03 23:47:42 -0300

    Clase 10 POO parte 3 Metodos set & get - tarea

--- Python/Leccion3/Persona2.py
+++ Python/Leccion3/Persona2.py
@@ -30,12 +30,12 @@ class Persona2:
         # print("Estamos utilizando el metodo get")
         return self._edad
 
-    """
+
     @edad.setter
     def edad(self, edad):  # Metodo setter
         # print("Estamos utilizando el metodo set")
         self._edad = edad
-    """
+
 
 persona1 = Persona2("Franco", "Pagano", 26)
 print(persona1.nombre) # de esta forma llamamos al metodo getter
@@ -45,3 +45,35 @@ print(persona1.mostrar_detalles())
 # persona1._edad = 40 ESTO NO SE HACE
 # Atributos read-only seria la edad porque no tiene el metodo set
 print(persona1.edad)
+# Tarea crear tres objetos mas, utilizando los metodos getter and setter
+# para modificar, y mostrar los cambios con el metodo mostrar detalles
+
+# Objeto numero uno de la tarea
+persona2 = Persona2("Rolando", "Ando", 13)
+persona2.nombre = "Franco"
+persona2.apellido = "Pagano"
+persona2.edad = 26
+print(persona2.nombre)
+print(persona2.apellido)
+print(persona2.edad)
+print(persona2.mostrar_detalles())
+
+# Objeto numero dos de la tarea
+persona3 = Persona2("Rolando", "Ando", 13)
+persona3.nombre = "Julieta"
+persona3.apellido = "Quiroga"
+persona3.edad = 27
+print(persona3.nombre)
+print(persona3.apellido)
+print(persona3.edad)
+print(persona3.mostrar_detalles())
+
+# Objeto numero tres de la tarea
+persona4 = Persona2("Rolando", "Ando", 13)
+persona4.nombre = "Juan Pablo"
+persona4.apellido = "Dell Pozzi"
+persona4.edad = 27
+print(persona4.nombre)
+print(persona4.apellido)
+print(persona4.edad)
+print(persona4.mostrar_detalles())
