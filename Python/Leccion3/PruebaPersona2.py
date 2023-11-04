8407f4742a59e73fb5a3d7522bb83e4f4003caf7
Franco <francopaganoo2015@gmail.com>
2023-11-04 00:00:29 -0300

    Clase 10 POO parte 3 Metodos set & get - Comprobacion del modulo principal en ejecucion

--- Python/Leccion3/PruebaPersona2.py
+++ Python/Leccion3/PruebaPersona2.py
@@ -1,4 +1,7 @@
 from Persona2 import Persona2
 
-persona1 = Persona2("Lionel", "Messi", 36)
-persona1.mostrar_detalles()
+if __name__ == "__main__":
+    persona1 = Persona2("Lionel", "Messi", 36)
+    persona1.mostrar_detalles()
+
+    print(__name__)
