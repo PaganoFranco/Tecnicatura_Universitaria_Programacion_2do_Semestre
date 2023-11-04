a73d4896c6a28c0d497941b0956a0a6e155fba6a
Franco <francopaganoo2015@gmail.com>
2023-11-03 23:31:55 -0300

    Clase 10 POO parte 3 Metodos set & get - Atributos read - only

--- Python/Leccion3/Persona2.py
+++ Python/Leccion3/Persona2.py
@@ -30,13 +30,18 @@ class Persona2:
         # print("Estamos utilizando el metodo get")
         return self._edad
 
+    """
     @edad.setter
     def edad(self, edad):  # Metodo setter
         # print("Estamos utilizando el metodo set")
         self._edad = edad
+    """
 
 persona1 = Persona2("Franco", "Pagano", 26)
 print(persona1.nombre) # de esta forma llamamos al metodo getter
 persona1.nombre = "Juan Pablo"
 print(persona1.nombre)
 print(persona1.mostrar_detalles())
+# persona1._edad = 40 ESTO NO SE HACE
+# Atributos read-only seria la edad porque no tiene el metodo set
+print(persona1.edad)
